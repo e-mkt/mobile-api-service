0f5819a3558f84257e4f9ca734acf0ece413ac8c
mraraujo <marquinhoaraujo@gmail.com>
2022-05-19 19:13:30 -0300

    chore: project initializer setup

---
+++ src/main/kotlin/me/training/mobileapiservice/MobileApiServiceApplication.kt
@@ -0,0 +1,11 @@
+package me.training.mobileapiservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MobileApiServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<MobileApiServiceApplication>(*args)
+}
---
+++ src/test/kotlin/me/training/mobileapiservice/MobileApiServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.training.mobileapiservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MobileApiServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
